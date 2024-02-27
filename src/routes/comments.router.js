9be710b82608cd87d78319bd218e6f1678c7474c
rosa1435 <158241447+rosa1435@users.noreply.github.com>
2024-02-27 15:22:44 +0900

    댓글 목록 조회 API완성(노 테스트)

--- src/routes/comments.router.js
+++ src/routes/comments.router.js
@@ -4,13 +4,13 @@ import { prisma } from '../utils/prisma/index.js';
 const router = express.Router();
 
 
-//router.post(('/', async (req, res) => {}));
+//router.post('/', async (req, res) => {});
 
 
 // 댓글 등록 API
 router.post('/reviews/:reviewId/comments', async (req, res) => {
     const {content, author, password} = req.body;
-    const {reviewId} = req.params
+    const {reviewId} = req.params;
 
 
     if(!content || !author || !password || !reviewId){
@@ -41,7 +41,39 @@ router.post('/reviews/:reviewId/comments', async (req, res) => {
 });
 
 
+// 댓글 목록 조회 API
+router.get('/reviews/:reviewId/comments', async (req, res) => {
+    const {reviewId} = req.params;
 
+
+    const findreviewId = await prisma.reviews.findUnique({ // reviews테이블안에 reviewId를 찾음
+        where: {
+            reviewId: +reviewId
+        }});
+
+        if (!findreviewId) { // reviewId가 존재하지 않을 경우 에러메셎지
+            return res.status(404).json({message: "존재하지 않는 리뷰입니다."});
+        }
+
+
+    const commentsList = await prisma.comments.findMany({ // reviewId가 일치하는 목록 생성
+        where: {
+            reviewId: reviewId,
+        },
+        select: {
+            id: true,
+            content: true,
+            author: true,
+            createdAt: true,
+            updatedAt:true,
+        },
+        orderBy: {
+            createdAt: 'desc', // 시간 최신 순으로 정렬
+        }
+    });
+
+    return res.status(200).json({commentsList}) // 목록 리턴
+});
 
 
 
