9ffc060800cd30ddf6b14f66a0131202be3c10cf
rosa1435 <158241447+rosa1435@users.noreply.github.com>
2024-02-27 14:30:13 +0900

    댓글등록 API 작성완료 (태스트는 못함)

--- src/routes/comments.router.js
+++ src/routes/comments.router.js
@@ -14,14 +14,20 @@ router.post('/reviews/:reviewId/comments', async (req, res) => {
 
 
     if(!content || !author || !password || !reviewId){
-        return res.status(400).json({errormessage: "데이터 형식이 올바르지 않습니다."}) // body 또는 params를 입력받지 못한 경우
+        return res.status(400).json({message: "데이터 형식이 올바르지 않습니다."}) // body 또는 params를 입력받지 못한 경우
     }
 
-    if(!reviewId){
-        return res.status(400).json({errormessage: "데이터 형식이 올바르지 않습니다."}) // body 또는 params를 입력받지 못한 경우
+    const findreviewId = await prisma.reviews.findUnique({ // reviews테이블안에 reviewId를 찾음
+        where: {
+            reviewId: +reviewId
+        }
+    });
+
+    if(!findreviewId){
+        return res.status(404).json({message: "존재하지 않는 리뷰입니다."}) // reviewId가 존재하지 않을 경우
     }
 
-    const comment = await prisma.Comments.create({ // 댓글을 데이터베이스에 push하는 부분
+    const comment = await prisma.comments.create({ // 댓글을 데이터베이스에 push하는 부분
         data : {
             content: content,
             author: author,
